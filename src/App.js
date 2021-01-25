46fa202e02831aeda66b90d267988af5396689a7
Prateek <pwahi23@gmail.com>
2021-01-25 18:45:03 +0530

    HTML Structure

--- src/App.js
+++ src/App.js
@@ -3,22 +3,101 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <section className="main-container">
+        <h2>SpaceX Launch Programs</h2>
+        <div className="main-wrapper">
+          <div className="filter-wrapper">
+              <strong>Filters</strong>
+          </div>
+          <div className="filter-content__wrapper">
+            <div className="filter-content__wrapper--card">
+              <div className="img-wrapper">
+              </div>
+              <div className="cards-content__wrapper">
+                <strong>FalconSat #1</strong>
+                <div className="mission-details">
+                  <p>
+                    <strong>Mission Id's</strong>
+                    <span>Telsa1, Tesla2</span>
+                  </p>
+                  <p></p>
+                  <p></p>
+                  <p></p>
+                </div>
+              </div>
+            </div>
+
+            <div className="filter-content__wrapper--card">
+              <div className="img-wrapper">
+              </div>
+              <div className="cards-content__wrapper">
+                <strong>FalconSat #1</strong>
+                <div className="mission-details">
+                  <p>
+                    <strong>Mission Id's</strong>
+                    <span>Telsa1, Tesla2</span>
+                  </p>
+                  <p></p>
+                  <p></p>
+                  <p></p>
+                </div>
+              </div>
+            </div>
+
+            <div className="filter-content__wrapper--card">
+              <div className="img-wrapper">
+                <img src="https://images2.imgbox.com/52/09/eNvilptL_o.png" alt="SpaceX" />
+              </div>
+              <div className="cards-content__wrapper">
+                <strong>FalconSat #1</strong>
+                <div className="mission-details">
+                  <p>
+                    <strong>Mission Id's:</strong>
+                    <span>Telsa1, Tesla2</span>
+                  </p>
+                  <p>
+                    <strong>Launch Year:</strong>
+                    <span>2020</span>
+                  </p>
+                  <p>
+                    <strong>SuccessFul Launch:</strong>
+                    <span>Success</span>
+                  </p>
+                  <p>
+                    <strong>SuccessFul Landing:</strong>
+                    <span>Success</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="filter-content__wrapper--card">
+              <div className="img-wrapper">
+              </div>
+              <div className="cards-content__wrapper">
+                <strong>FalconSat #1</strong>
+                <div className="mission-details">
+                  <p>
+                    <strong>Mission Id's</strong>
+                    <span>Telsa1, Tesla2</span>
+                  </p>
+                  <p></p>
+                  <p></p>
+                  <p></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </section>  
+    <footer>
+    <div className="footer">
+      <strong>Developed By: </strong>
+      <span>Prateek Wahi</span>
     </div>
+  </footer>
+  </>
   );
 }
 
